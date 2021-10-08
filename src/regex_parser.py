cf900af6118ffa48b03c779853f33ea9c62acc1d
Taras Rashkevych <taras.rashkevych1999@gmail.com>
2021-10-08 15:32:20 +0200

    add the first version of create_expression_tree(), add two new boolean methods, fix _preprocess_regex() and refactor the names

--- src/regex_parser.py
+++ src/regex_parser.py
@@ -1,3 +1,7 @@
+from typing import NoReturn
+from expression_tree import ExpressionTree
+
+
 class RegexParser:
     _REGEX_LEFT_PAR = "("
     _REGEX_RIGHT_PAR = ")"
@@ -8,20 +12,21 @@ class RegexParser:
     def __init__(self, regex: str) -> None:
         self._regex_in_infix_notation_raw = regex
         self._regex_in_infix_notation_preprocessed = self._preprocess_regex(regex)
-        self._regex_in_postfix_notation_parsed = ""
+        self._regex_in_postfix_notation = ""
+        self._expression_tree = None
 
     @property
-    def regex_in_infix_notation_raw(self):
+    def regex_in_infix_notation_raw(self) -> str:
         return self._regex_in_infix_notation_raw
 
     @property
-    def regex_in_infix_notation_preprocessed(self):
+    def regex_in_infix_notation_preprocessed(self) -> str:
         return self._regex_in_infix_notation_preprocessed
 
     @property
-    def regex_in_postfix_notation_parsed(self):
-        if self._regex_in_postfix_notation_parsed:
-            return self._regex_in_postfix_notation_parsed
+    def regex_in_postfix_notation(self) -> str:
+        if self._regex_in_postfix_notation:
+            return self._regex_in_postfix_notation
         else:
             raise ValueError(
                 "The regex has not been parsed. Please call the convert_from_infix_to_postfix() method."
@@ -34,22 +39,40 @@ class RegexParser:
             next_char = self._regex_in_infix_notation_preprocessed[i]
 
             if self._is_operand(next_char):
-                self._regex_in_postfix_notation_parsed += next_char
+                self._regex_in_postfix_notation += next_char
             elif next_char == RegexParser._REGEX_LEFT_PAR:
                 stack.append(RegexParser._REGEX_LEFT_PAR)
             elif next_char == RegexParser._REGEX_RIGHT_PAR:
                 while stack[-1] != RegexParser._REGEX_LEFT_PAR:
-                    self._regex_in_postfix_notation_parsed += stack.pop()
+                    self._regex_in_postfix_notation += stack.pop()
                 stack.pop()
             else:
                 while len(stack) != 0 and self._get_precedence(
                     next_char
                 ) <= self._get_precedence(stack[-1]):
-                    self._regex_in_postfix_notation_parsed += stack.pop()
+                    self._regex_in_postfix_notation += stack.pop()
                 stack.append(next_char)
 
         while len(stack) != 0:
-            self._regex_in_postfix_notation_parsed += stack.pop()
+            self._regex_in_postfix_notation += stack.pop()
+
+    def create_expression_tree(self) -> None:
+        stack = []
+
+        for character in self.regex_in_postfix_notation:
+            if self._is_kleene_star_operator(character):
+                left_child = stack.pop()
+                stack.append(ExpressionTree(character, left_child))
+            elif self._is_concat_operator(character) or self._is_alternation_operator(
+                character
+            ):
+                right_child = stack.pop()
+                left_child = stack.pop()
+                stack.append(ExpressionTree(character, left_child, right_child))
+            else:
+                stack.append(ExpressionTree(character))
+
+        self._expression_tree = stack[-1]
 
     def _preprocess_regex(self, regex: str) -> str:
         if not regex:
@@ -67,6 +90,10 @@ class RegexParser:
                 or (
                     self._is_kleene_star_operator(pred_char)
                     and self._is_operand(current_char)
+                )
+                or (
+                    self._is_kleene_star_operator(pred_char)
+                    and self._is_left_parentesis(current_char)
                 )
                 or (
                     self._is_left_parentesis(pred_char)
@@ -107,6 +134,12 @@ class RegexParser:
             and character != RegexParser._REGEX_RIGHT_PAR
         )
 
+    def _is_concat_operator(self, character: str) -> bool:
+        return character == RegexParser._REGEX_CONCAT_OP
+
+    def _is_alternation_operator(self, character: str) -> bool:
+        return character == RegexParser._REGEX_ALTERNATION_OP
+
     def _is_kleene_star_operator(self, character: str) -> bool:
         return character == RegexParser._REGEX_KLEENE_STAR_OP
 
@@ -125,6 +158,4 @@ if __name__ == "__main__":
         f"Preprocessed regex in infix notation:\t {parser.regex_in_infix_notation_preprocessed}"
     )
     parser.convert_from_infix_to_postfix_notation()
-    print(
-        f"Parsed regex in postfix notation:\t {parser.regex_in_postfix_notation_parsed}"
-    )
+    print(f"Parsed regex in postfix notation:\t {parser.regex_in_postfix_notation}")
