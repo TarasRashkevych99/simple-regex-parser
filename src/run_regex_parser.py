71c56ef466f9c5aa400ba6a8e0d383524957c04f
Taras Rashkevych <taras.rashkevych1999@gmail.com>
2021-10-20 23:43:51 +0200

    add the main script for running the regex parser

---
+++ src/run_regex_parser.py
@@ -0,0 +1,23 @@
+import argparse
+from regex_parser import RegexParser
+
+
+def parse_arguments() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description="Parse regular expressions and test words againt them"
+    )
+    parser.add_argument("regex", help="the non-empty regex to be parsed", type=str)
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    regex = args.regex
+    parser = RegexParser(regex)
+    print(f"Raw regex in infix notation:\t\t {parser.raw_regex}")
+    print(f"Purified regex in infix notation:\t {parser.purified_regex}")
+    print(f"Preprocessed regex in infix notation:\t {parser.preprocessed_regex}")
+    print(f"Converted regex in postfix notation:\t {parser.converted_regex}")
+    print(parser.expression_tree)
+    print(parser.nfa)
