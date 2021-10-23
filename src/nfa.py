6cc1b33c73084113b5e2a429f50bbe0ad9999931
Taras Rashkevych <taras.rashkevych1999@gmail.com>
2021-10-24 01:03:00 +0200

    add the new printing methods to display the '\' character without the second '\' character at the end

--- src/nfa.py
+++ src/nfa.py
@@ -18,9 +18,27 @@ class NFA:
         nfa_repr += f"\tInitial State: ({self.initial_state})\n"
         nfa_repr += f"\tFinal State: (({self.final_state}))\n"
         nfa_repr += f"\tStates({len(self.states)}): {self.states}\n"
-        nfa_repr += f"\tAlphabet({len(self.alphabet)}): {self.alphabet}\n"
-        nfa_repr += f"\tTransition Function({sum((len(self.trans_func[key]) for key in self.trans_func))}): "
-        for key in self.trans_func:
-            nfa_repr += f"\n{str(key).rjust(40)} --> {self.trans_func[key]}"
+        nfa_repr += f"\tAlphabet({len(self.alphabet)}): {self._print_alphabet()}\n"
+        nfa_repr += f"\tTransition Function({self._compute_number_of_edges()}): "
+        nfa_repr += self._print_trans_func()
 
         return nfa_repr
+
+    def _print_alphabet(self) -> str:
+        alphabet_repr = "{"
+        alphabet = sorted(self.alphabet)
+        alphabet_repr += ", ".join("'" + character + "'" for character in alphabet)
+        alphabet_repr += "}"
+
+        return alphabet_repr
+
+    def _print_trans_func(self) -> str:
+        trans_func_repr = ""
+        for key in self.trans_func:
+            key_repr = f"({key[0]}, '{key[1]}')"
+            trans_func_repr += f"\n{key_repr.rjust(40)} --> {self.trans_func[key]}"
+
+        return trans_func_repr
+
+    def _compute_number_of_edges(self) -> int:
+        return sum((len(self.trans_func[key]) for key in self.trans_func))
