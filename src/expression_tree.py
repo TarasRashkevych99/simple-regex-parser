7b50527969a6cda63cb6ab108289cda2237eb05e
Taras Rashkevych <taras.rashkevych1999@gmail.com>
2021-10-24 23:11:25 +0200

    update the signature of the __init__() method

--- src/expression_tree.py
+++ src/expression_tree.py
@@ -1,9 +1,14 @@
 from __future__ import annotations
 
+from regex_parser import Character
+
 
 class ExprTree:
     def __init__(
-        self, character: str, left_child: ExprTree = None, right_child: ExprTree = None
+        self,
+        character: Character,
+        left_child: ExprTree = None,
+        right_child: ExprTree = None,
     ) -> None:
         self.left_child = left_child
         self.right_child = right_child
